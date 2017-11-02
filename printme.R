e2da298fd869c40c4b5d73dd57885685efba503d
Michael Graber <m.graber@campus.lmu.de>
2017-11-02 23:05:28 +0100

    improved printme

--- printme.R
+++ printme.R
@@ -1,4 +1,6 @@
 name = "<your name>"
 eyecolor = "<your eyevcolor>"
 meal = "<your favorite meal>"
-print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+printer <- function(name, eyecolor, meal){
+  print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+}
